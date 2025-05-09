6033d792060cf73254c7fb2430b39a05518a17bc
Dmitry <neox56@gmail.com>
2025-05-09 14:05:29 +0400

    4.4. Создание форматированных строк Строки и функции

--- main.go
+++ main.go
@@ -9,15 +9,13 @@ func main() {
 	const IMTPower = 2
 	var userHeight float64 // 0.0
 	var userKg float64     // 0.0
-	// 	fmt.Println(`__Калькулятор индекса массы тела__
-	// Введите свой рост в сантиметрах:
-	// 	`)
+
 	fmt.Println("__Калькулятор индекса массы тела__")
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите ваш вес в кг: ")
 	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight/100, IMTPower)
-	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
-	// fmt.Print("Ваш индекс массы тела: ", IMT)
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
+	fmt.Print(result)
 }
