71dcb28176b82715bac8c9e333f0ff12037652cf
Dmitry <neox56@gmail.com>
2025-05-09 19:52:16 +0400

    Проект расчет индекса массы тела

---
+++ Проект расчет индекса массы тела/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+const IMTPower = 2
+
+func main() {
+
+	fmt.Println("__Калькулятор индекса массы тела__")
+	for {
+		userHeight, userKg := getUserInput()
+		IMT, error := calculateIMT(userHeight, userKg)
+		if error != nil {
+			// fmt.Println("Не заданы параметры для расчета")
+			// continue
+			panic("Не заданы параметры для расчета")
+		}
+		outputResult(IMT)
+
+		isRepeatCalculation := checkRepeatCalculation()
+
+		if !isRepeatCalculation {
+			break
+		}
+	}
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
+	fmt.Println(result)
+	switch {
+	case imt < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case imt < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case imt < 25:
+		fmt.Println("У вас нормальный вес")
+	case imt < 30:
+		fmt.Println("У вас избыточный вес")
+	default:
+		fmt.Println("У вас степень ожирения")
+	}
+}
+
+func calculateIMT(userHeight float64, userKg float64) (float64, error) {
+	if userHeight <= 0 || userKg <= 0 {
+		return 0, errors.New("NO_PARAMS_ERROR")
+	}
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+
+	return IMT, nil
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64 // 0.0
+	var userKg float64     // 0.0
+
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите ваш вес в кг: ")
+	fmt.Scan(&userKg)
+
+	return userHeight, userKg
+}
+
+func checkRepeatCalculation() bool {
+	var userChoise string
+	fmt.Print("Вы хотите сделать еще раз расчет (y/n): ")
+	fmt.Scan(&userChoise)
+
+	if userChoise == "y" || userChoise == "Y" {
+		return true
+	}
+
+	return false
+}
