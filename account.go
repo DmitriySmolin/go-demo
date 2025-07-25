22b3d8b43ba15dc6b042eaa1549e8cdebfd26389
Dmitry <neox56@gmail.com>
2025-07-25 22:14:49 +0400

    10.1. Разделение кода Пакеты

---
+++ account.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+type accountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	account
+	// acc account
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
+func newAccountWithTimeStamp(login string, password string, urlString string) (*accountWithTimeStamp, error) {
+
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAcc := &accountWithTimeStamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			login:    login,
+			password: password,
+			url:      urlString,
+		},
+	}
+
+	if password == "" {
+		newAcc.generatePassword(12) // более упрощенный вызов встроенного метода
+		// newAcc.account.generatePassword(12) // допустимое обращение при композиции
+	}
+
+	return newAcc, nil
+}
+
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+
+	acc.password = string(res)
+}
