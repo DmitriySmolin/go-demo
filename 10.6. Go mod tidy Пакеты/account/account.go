dc7bdd91924d769f3d122f949901315696fcf941
neox56 <neox56@gmail.com>
2025-07-27 14:51:12 +0400

    10.6. Go mod tidy Пакеты

---
+++ 10.6. Go mod tidy Пакеты/account/account.go
@@ -0,0 +1,70 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Account struct {
+	login    string
+	password string
+	url      string
+}
+
+type AccountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	Account
+	// acc account
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
+func NewAccountWithTimeStamp(login string, password string, urlString string) (*AccountWithTimeStamp, error) {
+
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAcc := &AccountWithTimeStamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		Account: Account{
+			login:    login,
+			password: password,
+			url:      urlString,
+		},
+	}
+
+	if password == "" {
+		newAcc.generatePassword(12) // более упрощенный вызов встроенного метода
+		// newAcc.account.generatePassword(12) // допустимое обращение при композиции
+	}
+
+	return newAcc, nil
+}
+
+func (acc *Account) OutputPassword() {
+	color.Cyan(acc.login)
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *Account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+
+	acc.password = string(res)
+}
---
+++ 10.6. Go mod tidy Пакеты/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go-demo/account"
+	"go-demo/files"
+)
+
+func main() {
+
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL ")
+
+	myAccount, err := account.NewAccountWithTimeStamp(login, password, url)
+
+	if err != nil {
+		fmt.Println("Неверный формат URL или Логин")
+		return
+	}
+
+	myAccount.OutputPassword()
+	files.WriteFile()
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
+}
