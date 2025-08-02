6779378f5c1a12ac91790ec6ff12abf821d5bfe0
Dmitry <neox56@gmail.com>
2025-08-02 11:42:15 +0400

    11.3. Defer Работа с ОС

---
+++ 11.3. Defer Работа с ОС/files/files.go
@@ -0,0 +1,30 @@
+package files
+
+import (
+	"fmt"
+	"os"
+)
+
+func ReadFile() {
+
+}
+
+func WriteFile(content string, name string) {
+	file, err := os.Create(name)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Запись успешна")
+
+}
---
+++ 11.3. Defer Работа с ОС/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"go-demo/account"
+	"go-demo/files"
+)
+
+func main() {
+	// defer fmt.Println(1) // 1
+	// defer fmt.Println(2) // 2
+	// порядок вывода 2 -> 1 // по приниципу LIFO работает defer
+	files.WriteFile("Привет! Я файл!!", "file.txt")
+
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL ")
+
+	myAccount, err := account.NewAccountWithTimeStamp(login, password, url)
+
+	if err != nil {
+		fmt.Println("Неверный формат URL или Логин")
+		return
+	}
+
+	myAccount.OutputPassword()
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
+}
